b13920b625dc4ee11e3926e5c1ab81280c0823c0
rishab460 <rishab.2101103ec@iiitbh.ac.in>
2024-08-09 18:48:07 +0530

    added template for segment tree

--- segmentTree.cpp
+++ segmentTree.cpp
@@ -1,5 +1,23 @@
 class SGTree {
 	vector<int> seg;
+    int _query(int ind, int low, int high, int l, int r) {
+		if (r < low || high < l) return 0;
+		if (low >= l && high <= r) return seg[ind];
+		int mid = (low + high) >> 1;
+		int left = _query(2 * ind + 1, low, mid, l, r);
+		int right = _query(2 * ind + 2, mid + 1, high, l, r);
+		return left + right;
+	}
+    void _update(int ind, int low, int high, int i, int val) {
+		if (low == high) {
+			seg[ind] = val;
+			return;
+		}
+		int mid = (low + high) >> 1;
+		if (i <= mid) _update(2 * ind + 1, low, mid, i, val);
+		else _update(2 * ind + 2, mid + 1, high, i, val);
+		seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
+	}
 public:
 	SGTree(vector<int> &arr) {
         int n = arr.size();
@@ -19,25 +37,7 @@ public:
     int query(int l, int r) {
         return _query(0, 0, seg.size()/4-1, l, r);
     }
-	int _query(int ind, int low, int high, int l, int r) {
-		if (r < low || high < l) return 0;
-		if (low >= l && high <= r) return seg[ind];
-		int mid = (low + high) >> 1;
-		int left = _query(2 * ind + 1, low, mid, l, r);
-		int right = _query(2 * ind + 2, mid + 1, high, l, r);
-		return left + right;
-	}
     void update(int i, int val) {
         _update(0, 0, seg.size()/4-1, i, val);
     }
-	void _update(int ind, int low, int high, int i, int val) {
-		if (low == high) {
-			seg[ind] = val;
-			return;
-		}
-		int mid = (low + high) >> 1;
-		if (i <= mid) _update(2 * ind + 1, low, mid, i, val);
-		else _update(2 * ind + 2, mid + 1, high, i, val);
-		seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
-	}
 };
